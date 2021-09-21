959eb2cf1c2d1400fdd024e17d298faa65db438c
gabrielcoronin <gabrielcoronin@gmail.com>
2021-09-21 19:27:16 -0300

    feat: add payments

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -1,28 +1,26 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 import 'package:rushtowin/models/recharge_model.dart';
 import 'package:rushtowin/models/transaction.dart';
 import 'package:rushtowin/models/wallet.dart';
-import 'package:uuid/uuid.dart';
 
 class TransactionWebClient {
-
   Future<List<Transaction>> list(String walletId) async {
-    final response = await http.get(
-        Uri.parse('http://192.168.5.185:8080/api/Transactions/$walletId'));
+    final response = await http
+        .get(Uri.parse('http://192.168.5.185:8080/api/Transactions/$walletId'));
     final List<dynamic> decodedJson = jsonDecode(response.body);
     if (response.statusCode == 200) {
-      return decodedJson.map((dynamic json) => Transaction.fromJson(json))
+      return decodedJson
+          .map((dynamic json) => Transaction.fromJson(json))
           .toList();
     }
     throw Exception('Failed to load transaction');
   }
 
   Future<Wallet> getWallet(String walletId) async {
-    final response = await http.get(
-        Uri.parse('http://192.168.5.185:8080/api/Transactions/getWallet/$walletId'));
+    final response = await http.get(Uri.parse(
+        'http://192.168.5.185:8080/api/Transactions/getWallet/$walletId'));
 
     if (response.statusCode == 200) {
       return Wallet.fromJson(jsonDecode(response.body));
@@ -31,15 +29,14 @@ class TransactionWebClient {
   }
 
   Future<Transaction> getLastTransaction(String walletId) async {
-    final response = await http.get(
-        Uri.parse('http://192.168.5.185:8080/api/Transactions/lastTransaction/$walletId'));
+    final response = await http.get(Uri.parse(
+        'http://192.168.5.185:8080/api/Transactions/lastTransaction/$walletId'));
 
     if (response.statusCode == 200) {
       return Transaction.fromJson(jsonDecode(response.body));
     }
     throw Exception('Failed to retrieve last transaction');
   }
-
 
   Future<Transaction> recharge(RechargeModel recharge) async {
     final String rechargeJson = jsonEncode(recharge.toJson());
@@ -58,15 +55,14 @@ class TransactionWebClient {
     throw Exception('Failed to load transaction');
   }
 
-  Future<Transaction> bus(Uuid walletId) async {
+  Future<Transaction> bus(String walletId) async {
     await Future.delayed(const Duration(seconds: 2));
 
     final Response response = await http.post(
-        Uri.parse('http://192.168.5.185:8080/api/Transactions/bus'),
+        Uri.parse('http://192.168.5.185:8080/api/Transactions/bus/$walletId'),
         headers: {
           'Content-type': 'application/json',
-        }, body: walletId)
-    ;
+        });
 
     if (response.statusCode == 200) {
       return Transaction.fromJson(jsonDecode(response.body));
@@ -74,16 +70,15 @@ class TransactionWebClient {
     throw Exception('Failed to load transaction');
   }
 
-  Future<Transaction> subway(Uuid walletId) async {
+  Future<Transaction> subway(String walletId) async {
     await Future.delayed(const Duration(seconds: 2));
 
     final Response response = await http.post(
-        Uri.parse('http://192.168.5.185:8080/api/Transactions/subway'),
+        Uri.parse(
+            'http://192.168.5.185:8080/api/Transactions/subway/$walletId'),
         headers: {
           'Content-type': 'application/json',
-        },
-        body: walletId)
-    ;
+        });
 
     if (response.statusCode == 200) {
       return Transaction.fromJson(jsonDecode(response.body));
@@ -91,16 +86,14 @@ class TransactionWebClient {
     throw Exception('Failed to load transaction');
   }
 
-  Future<Transaction> train(Uuid walletId) async {
+  Future<Transaction> train(String walletId) async {
     await Future.delayed(const Duration(seconds: 2));
 
     final Response response = await http.post(
-        Uri.parse('http://192.168.5.185:8080/api/Transactions/train'),
+        Uri.parse('http://192.168.5.185:8080/api/Transactions/train/$walletId'),
         headers: {
           'Content-type': 'application/json',
-        },
-        body: walletId)
-    ;
+        });
 
     if (response.statusCode == 200) {
       return Transaction.fromJson(jsonDecode(response.body));
